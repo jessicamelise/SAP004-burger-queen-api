bec67ca34de6e20b8cedfec0fb3d6ec8cdaeb214
jessicamelise <jessica.melise@hotmail.com>
2020-08-28 14:50:00 -0300

    implementação de deploy

--- api/server/src/models/index.js
+++ api/server/src/models/index.js
@@ -1,7 +1,11 @@
+'use strict';
+
 import fs from 'fs'
 import path from 'path'
 import Sequelize from 'sequelize'
 import configJson from '../config/config'
+
+require('dotenv').config();
 
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV ? process.env.NODE_ENV : 'development'
